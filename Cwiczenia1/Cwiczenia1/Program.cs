11100ccaa18d0d786fdb6897f17a7fe52ea4f9a7
s19562 <s19562@pjwstk.edu.pl>
2020-03-02 11:32:50 +0100

    Dodalem projekt

---
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cwiczenia1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
