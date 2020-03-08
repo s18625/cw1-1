4f59a424a93b6d37739c86b829abe83a3e0666fb
s19562 <49717167+s19562@users.noreply.github.com>
2020-03-08 16:13:06 +0100

    Update Program.cs

--- my1/my1/Program.cs
+++ my1/my1/Program.cs
@@ -1,21 +1,34 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace my1
 {
     public class Program
     {
+       
         public static async Task Main(string[] args)
         {
+            var pattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
+            var webPage = args[0];
 
             var httpClient = new HttpClient();
-            var response = await httpClient.GetAsync(args[0]);
+            var response = await httpClient.GetAsync(webPage);
+            var content = response.Content;
+            var result = await content.ReadAsStringAsync();
 
-            // ...
-
-
-
+            var regex = new Regex(pattern);
+            foreach (var word in result.Split(' '))
+            {
+                var match = regex.Match(word);
+                if (match.Success)
+                {
+                    Console.WriteLine(word);
+                }
+            }
         }
     }
 }
+
+
