3cab16140bcd64556cb63a243b1a9ccd171c9aab
s19562 <49717167+s19562@users.noreply.github.com>
2020-03-08 15:41:47 +0100

    Create my1

---
+++ my1/my1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace my1
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(args[0]);
+
+            // ...
+
+
+
+        }
+    }
+}
